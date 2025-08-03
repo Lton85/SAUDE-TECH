03c6ba86c00947850830db83152a665e8419cf34
Bruno Carrasco <carrasco.bruno@gmail.com>
2025-08-03 01:28:44 +0000

    mostrar notificação no topo da tela

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -29,7 +29,7 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport />
+      <ToastViewport className="sm:top-0 sm:right-auto sm:left-1/2 sm:-translate-x-1/2" />
     </ToastProvider>
   )
 }
