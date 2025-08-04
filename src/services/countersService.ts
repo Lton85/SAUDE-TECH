9cdf9dff59212f20392e93ed2fcf1440985b6c0c
Bruno Carrasco <carrasco.bruno@gmail.com>
2025-08-04 02:05:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Badge variant="ou

--- src/services/countersService.ts
+++ src/services/countersService.ts
@@ -1,20 +1,29 @@
 "use client"
 import { db } from '@/lib/firebase';
-import { doc, getDoc, setDoc, updateDoc, increment } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc, increment, runTransaction } from 'firebase/firestore';
 
 // Obtém o próximo número do contador para uma coleção específica
 export const getNextCounter = async (collectionName: string): Promise<number> => {
     const counterDocRef = doc(db, 'counters', collectionName);
-    const counterDoc = await getDoc(counterDocRef);
 
-    if (!counterDoc.exists()) {
-        // Se o contador não existir, cria um novo com valor inicial
-        await setDoc(counterDocRef, { nextId: 1 });
-        return 1;
-    } else {
-        // Se o contador já existir, incrementa o valor e o retorna
-        await updateDoc(counterDocRef, { nextId: increment(1) });
-        const updatedCounter = await getDoc(counterDocRef);
-        return updatedCounter.data()?.nextId;
+    try {
+        const nextId = await runTransaction(db, async (transaction) => {
+            const counterDoc = await transaction.get(counterDocRef);
+
+            if (!counterDoc.exists()) {
+                // Se o contador não existir, inicializa com 1 e retorna 1.
+                transaction.set(counterDocRef, { nextId: 2 });
+                return 1;
+            }
+
+            const currentId = counterDoc.data().nextId;
+            transaction.update(counterDocRef, { nextId: increment(1) });
+            return currentId;
+        });
+        return nextId;
+    } catch (error) {
+        console.error("Transaction failed: ", error);
+        throw error;
     }
 };
+
