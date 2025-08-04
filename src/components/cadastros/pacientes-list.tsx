fdaff8f12b88fe949f6016baabd67d232f3a8b85
Bruno Carrasco <carrasco.bruno@gmail.com>
2025-08-04 20:59:24 +0000

    sempre que finalizar um cadastro de paciente manter no menu pacientes na

--- src/components/cadastros/pacientes-list.tsx
+++ src/components/cadastros/pacientes-list.tsx
@@ -78,7 +78,6 @@ export function PacientesList() {
   const handleSuccess = () => {
     fetchData();
     setSelectedPatient(null);
-    router.push('/');
   };
 
   const handleAddNew = () => {
@@ -287,5 +286,3 @@ export function PacientesList() {
     </>
   );
 }
-
-    
