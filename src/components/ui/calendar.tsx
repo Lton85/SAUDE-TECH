596a595873de29f17c7306933a3db4998b652f7e
Bruno Carrasco <carrasco.bruno@gmail.com>
2025-08-05 14:05:01 +0000

    AO CLICAR NO CAMPO DA SETA DAR OPÇÕES DE BUSCAR FACILMENTE POR ANO

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,6 +25,7 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
+        caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -54,6 +55,9 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      captionLayout="dropdown-buttons"
+      fromYear={new Date().getFullYear() - 10}
+      toYear={new Date().getFullYear() + 10}
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
