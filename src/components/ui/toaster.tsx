071b1fcca1b72678af0766305d90abbcfd40a9c0
Bruno Carrasco <carrasco.bruno@gmail.com>
2025-08-08 20:03:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -46,7 +46,7 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport className="sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2" />
+      <ToastViewport />
     </ToastProvider>
   )
 }
